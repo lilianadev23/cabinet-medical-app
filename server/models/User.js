bd81b9b83e5c5c80c67056a1468b5d71e26298a9
marwa <marwa.berkane1@gmail.com>
2025-07-31 14:24:06 +0100

    modification User.js

--- server/models/User.js
+++ server/models/User.js
@@ -3,15 +3,43 @@ const { Schema } = mongoose;
 
 const BaseUserSchema = new Schema({
   nom: { type: String, required: true },
+  prenom: { type: String, required: true }, 
   email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
+  motdepasse: { type: String, required: true },
+  statut: { type: String, default: "actif" },
+  permission: { type: String }, 
+  role: { type: String, required: true } 
 }, { discriminatorKey: 'role', timestamps: true });
+
 
 const User = mongoose.model('User', BaseUserSchema);
 
-const AdminFields = new Schema({ permissions: [String] }, { _id: false });
-const MedecinFields = new Schema({ specialite: String, patients: [String] }, { _id: false });
-const SecretaireFields = new Schema({ bureau: String }, { _id: false });
+const AdminFields = new Schema({ 
+  permissions: [String] },
+   { _id: false });
+
+
+
+const MedecinFields = new Schema({
+   dateDebut: Date,
+   specialite: String, 
+   telephone: Number,
+   adresse: String,
+   cabinet: String,
+   patients: [String] }, 
+   { _id: false });
+
+
+const SecretaireFields = new Schema({ 
+  telephone: Number,
+  dateEmbauche: Date,
+  dateNaissance: Date,
+  niveauEtude: String,
+  numCompteBancaire: String,
+  numCnss: String,
+  cin: String,
+  bureau: String },
+   { _id: false });
 
 const Admin = User.discriminator('admin', AdminFields);
 const Medecin = User.discriminator('medecin', MedecinFields);
